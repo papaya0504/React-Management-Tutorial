94e225ef234d153f4901b9caa4d76f9c544ebd3f
papaya0504 <smj5560@daum.net>
2021-05-23 19:43:27 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '신미진',
+  'birthday': '921222',
+  'gender': '여자',
+  'job': '직장인'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '김동휘',
+  'birthday': '851222',
+  'gender': '남자',
+  'job': '사업가'
+}]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
