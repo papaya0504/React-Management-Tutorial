d552f2d7bdd7a3cc4fd890eeea6aeee5cb49a17f
papaya0504 <smj5560@daum.net>
2021-05-23 21:57:48 +0900

    Add Get Customers RESTAPI

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import {React, Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
 import Paper from '@material-ui/core/Paper';
@@ -18,54 +19,50 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '신미진',
-  'birthday': '921222',
-  'gender': '여자',
-  'job': '직장인'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '김동휘',
-  'birthday': '851222',
-  'gender': '남자',
-  'job': '사업가'
-}]
+class App extends Component {
 
-function App(props) {
-  const { classes } = props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map(c => {return (<Customer key={c.id} id={c.id}image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render(){
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c => {
+              return (<Customer key={c.id} id={c.id}image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+            }) : ""}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-});
+app.get('/api/customers', (req, res) => {
+    res.send([{
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '홍길동',
+        'birthday': '961222',
+        'gender': '남자',
+        'job': '대학생'
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '신미진',
+        'birthday': '921222',
+        'gender': '여자',
+        'job': '직장인'
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '김동휘',
+        'birthday': '851222',
+        'gender': '남자',
+        'job': '사업가'
+      }]
+    );
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
